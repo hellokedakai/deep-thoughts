96056858144f0e838da62ff22f655e12ac33aca9
Kedakai <nicoleteeth@ucla.edu>
2021-07-12 00:58:35 -0700

    add jwt web token & user authorization

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -28,10 +29,11 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
-
-            return user;    
-        },
-        login: async (parent, { email, password }) => {
+            const token = signToken(user);
+          
+            return { token, user };
+          },
+          login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
           
             if (!user) {
@@ -44,7 +46,8 @@ const resolvers = {
               throw new AuthenticationError('Incorrect credentials');
             }
           
-            return user;
+            const token = signToken(user);
+            return { token, user };
           }
     }    
 }
