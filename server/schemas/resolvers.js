edcf2a1dce5baf34d4921a4379027a3f24ca1cc0
Kedakai <nicoleteeth@ucla.edu>
2021-07-13 00:43:58 -0700

    implement auth middleware to populate me query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,18 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+          if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+              .select('-__v -password')
+              .populate('thoughts')
+              .populate('friends');
+        
+            return userData;
+          }
+        
+          throw new AuthenticationError('Not logged in');
+        },
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
@@ -32,23 +44,23 @@ const resolvers = {
             const token = signToken(user);
           
             return { token, user };
-          },
-          login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-          
-            if (!user) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-          
-            const correctPw = await user.isCorrectPassword(password);
-          
-            if (!correctPw) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-          
-            const token = signToken(user);
-            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+        
+          if (!user) {
+            throw new AuthenticationError('Incorrect credentials');
           }
+        
+          const correctPw = await user.isCorrectPassword(password);
+        
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+        
+          const token = signToken(user);
+          return { token, user };
+        }
     }    
 }
   
